02910a405732ddf3078c220fb2a8e694c4424085
Dezarea Bryan <dezareabryan@gmail.com>
2023-08-03 11:12:03 -0400

    Setup database with users and passwords tables. Added dummy data to test and all is functioning as is expected. Created sample users route to test -- tests ok.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
+require('dotenv').config();
 const express = require('express')
-
+const pgp = require('pg-promise')();
 
 const app = express()
 const PORT = 3003
+const db = pgp(process.env.DB)
 
 app.get('/heartbeat', (req,res) => {
     res.send('Heartbeat Steady')
+})
+
+app.get('/users', async (req, res) => {
+    const userData = await db.manyOrNone('SELECT * FROM users')
+    res.json(userData)
 })
 
 app.listen(PORT, () => {
