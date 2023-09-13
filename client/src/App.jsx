5e1caa4494f3e8bc3226f9357059ca201f6442a2
Dezarea Bryan <dezareabryan@gmail.com>
2023-09-13 11:40:16 -0400

    Added basic navbar - no functionality at all. Implemented bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './Navbar';
 import Register from './Register';
 import Login from './Login';
 import Dashboard from './Dashboard'; 
 import AddPasswordForm from './AddPasswordForm'
 import { TokenProvider } from './Token';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <TokenProvider> 
+      <Navbar />
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -17,7 +17,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:3003/signup', {
+      const response = await fetch('http://localhost:3000/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
