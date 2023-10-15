861cbdc78f4854947e38773c9548a43c973fd915
Dezarea Bryan <dezareabryan@gmail.com>
2023-10-15 17:05:01 -0400

    added avatar options and styled the registration page. Next working on dashboard.

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -10,18 +10,19 @@ const Register = () => {
     first_name: '',
     last_name: '',
     username: '',
-    avatar: '/placeholder.png',
+    avatar: '',
   });
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const avatars = [
-    '/placeholder.png',
-    '/fox.jpg',
-    '/placeholder.png',
-    '/placeholder.png',
-    '/placeholder.png',
-    '/placeholder.png',
+    '/penguin.svg',
+    '/sheep.png',
+    '/house-cat.svg',
+    '/giraffe.svg',
+    '/elephant.png',
+    '/eggplant.png',
+    '/battle-cat.svg',
   ];
 
   useEffect(() => {
@@ -52,7 +53,6 @@ const Register = () => {
       });
       const data = await response.json();
       console.log(data);
-      // Redirect to login page after successful registration
       window.location.replace('/login');
     } catch (error) {
       console.error('Error:', error);
@@ -60,51 +60,56 @@ const Register = () => {
   };
 
   return (
-    <div className="flex flex-col items-center bg-gray-100 py-10">
-      <div className="w-full md:w-2/3 p-6 bg-white rounded shadow-md">
+    <div className="flex flex-col items-center bg-gray-100 py-5">
+      <div className="w-full md:w-2/3 p-4 bg-white rounded shadow-md">
         <h2 className="text-2xl mb-4 text-center text-navy">Why Register?</h2>
-        <p className="text-gray-700 text-center mb-8">
+        <p className="text-gray-700 text-center mb-4">
           Our platform enables you to securely store and manage all your passwords in one place. Say goodbye to forgotten passwords and the hassle of insecure password storage. With our intuitive and secure solution, you can access your passwords whenever you need them, from any device. Your data remains encrypted and protected, ensuring your information stays safe and easily accessible only to you.
         </p>
         <form onSubmit={handleSubmit} className="flex flex-col">
           <input
+            required
             type="email"
             placeholder="Email"
             value={userData.email}
             onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-4 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
           />
           <input
+            required
             type="password"
             placeholder="Password"
             value={userData.pw}
             onChange={(e) => setUserData({ ...userData, pw: e.target.value })}
-            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-4 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
           />
           <input
+            required
             type="text"
             placeholder="First Name"
             value={userData.first_name}
             onChange={(e) => setUserData({ ...userData, first_name: e.target.value })}
-            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-4 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
           />
           <input
+            required
             type="text"
             placeholder="Last Name"
             value={userData.last_name}
             onChange={(e) => setUserData({ ...userData, last_name: e.target.value })}
-            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-4 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
           />
           <input
+            required
             type="text"
             placeholder="Username"
             value={userData.username}
             onChange={(e) => setUserData({ ...userData, username: e.target.value })}
-            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-4 focus:outline-none focus:ring-2 focus:ring-gray-400"
+            className="w-full border border-gray-300 rounded-md py-2 px-3 mb-2 focus:outline-none focus:ring-2 focus:ring-gray-400"
           />
-          <div className="flex flex-col mb-4 items-center">
-            <label htmlFor="avatar" className="text-sm font-semibold text-gray-700 mb-2">
-              Choose your avatar:
+          <div className="flex flex-col mb-2 items-center">
+            <label htmlFor="avatar" className="text-sm font-semibold text-gray-700 mr-2">
+              Click on the image to choose your avatar:
             </label>
             <button
               type="button"
@@ -114,7 +119,6 @@ const Register = () => {
               <img src={userData.avatar} className="rounded-full w-12 h-12" alt="Selected Avatar" />
             </button>
           </div>
-
           <button
             className="w-full bg-gray-800 text-white py-2 px-4 rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
             type="submit"
@@ -123,7 +127,7 @@ const Register = () => {
           </button>
         </form>
       </div>
-      <p className="mt-4">
+      <p className="mt-2">
         Already have an account?{' '}
         <a href="/login" className="text-blue-500">
           Login
